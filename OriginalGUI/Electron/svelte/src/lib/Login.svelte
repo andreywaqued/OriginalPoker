41dbcf7e7599de83bd7ea23eaba1238ff2efd0a8
danielgaban <91639510+danielgaban@users.noreply.github.com>
2023-10-22 19:31:47 -0400

    refactor(login): refactored login frontend and added buttons style go global()

--- OriginalGUI/Electron/svelte/src/lib/Login.svelte
+++ OriginalGUI/Electron/svelte/src/lib/Login.svelte
@@ -1,51 +1,75 @@
 <script>
-    let login = ""
-    let password = ""
+	import { enhance } from "$app/forms";
+    /**
+     *@type {("signin"|"signup")}
+     */
+    export let tipo   /**@type {string} */
     import { onMount } from 'svelte';
     let api
+
     onMount(() => {
         if (window.api) {
             api = window.api
         }
     });
-    function sendSignIn() {
-        api.send("signIn", {user: login, password: password})
+
+    /** @param {SubmitEvent} event */
+    function handleLogin(event) {
+        event.preventDefault()
+        const data = new FormData(event.target);
+        const user = data.get("username")
+        const password = data.get("password")
+        if (tipo === "signin") return api.send("signIn", {user, password})
+        const confirmPassword = data.get("confirm_password")
+        if (password !== confirmPassword) return "TODO! Need to show error to the client (clientside verification)"
+        const email = data.get("email")
+        // if (tipo === "signup") api.send("signUp", {user: username, password: password})
     }
+;
+
 </script>
 
 <style lang="scss">
-    button {
-        all: unset;
-    }
-    button:active {
-        transform: scale(0.95);
-    }
-    .loginDiv {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        height: 100%;
-        gap: 0.5em;
+    form {
         input {
-            text-align: center;
+            display: block;
+            border: unset;
+            border-bottom: 1px solid grey;
+            padding-bottom: 0.4em;
+            margin: 1em auto;
+            width: 100%;
+            font-size: 1.2em;
         }
-        .loginButton {
-            // background-color: white;
-            border-radius: 5px;
-            border: 1px solid white;
-            width: 25%;
-            height: 5%;
-            text-align: center;
-
+        input::-webkit-input-placeholder {
+            color: darkgray;
+        }
+        input:focus {
+            outline: none
+        }
+        div {
+            display: flex;
+            flex-direction: column;
+            gap: 0.5em;
         }
     }
 </style>
-
-<div class="loginDiv">
-    Login <input placeholder="Login" bind:value={login} on:input={(e)=>{console.log(e)}}/>
-    Password <input placeholder="Password" bind:value={password}/>
-    <button class="loginButton" on:click={sendSignIn}>SIGN IN</button>
-    <button class="loginButton">SIGN UP</button>
-</div>
+<form on:submit={handleLogin} use:enhance>
+    <input placeholder="Username" name="username" type="text"/>
+    <input placeholder="Password" name="password" type="password"/>
+    {#if tipo === "signin"}
+      <div>
+          <button class="roundedButton filled" type="submit">
+              Login
+          </button>
+      </div>
+    {:else if tipo === "signup"}
+      <input placeholder="Confirm Password" name="confirm_password" type="password"/>
+      <input placeholder="E-mail" name="email" type="email"/>
+      <div>
+          <button class="roundedButton filled" type="submit">
+              Signup
+          </button>
+      </div>
+    {/if}
+</form>
+<slot/>
