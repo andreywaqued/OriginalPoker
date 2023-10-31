12a922c1f022e645e2c05d31da0236fbbea032c6
Andrey Waqued <andrey.w@live.com>
2023-10-31 11:47:15 -0400

    fixed wrong path on server.js

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -12,19 +12,19 @@ const logger = new Logger("Server")
 //   connectionString: 'postgresql://postgres:dbpass@db:5432/original_poker'
 // })
 //internal render acess
-// fastify.register(require('@fastify/postgres'), {
-//   connectionString: 'postgres://original:fSuZdEE7T6fTqVCOlEobSioKlfwR4Rrb@dpg-ckdeitsgonuc73cmsucg-a/original_db'
-// })
-// fastify.register(require('@fastify/redis'), {
-//   url: 'redis://red-cksjdg6nfb1c73c8tgpg:6379'
-// })
-//external render acess
 fastify.register(require('@fastify/postgres'), {
-    connectionString: 'postgres://original:fSuZdEE7T6fTqVCOlEobSioKlfwR4Rrb@dpg-ckdeitsgonuc73cmsucg-a.oregon-postgres.render.com/original_db?ssl=true'
-  })
-fastify.register(require('@fastify/redis'), {
-  url: 'rediss://red-cksjdg6nfb1c73c8tgpg:eEjoQXin0xOlVfhsOu26xy3BpIjjdgul@oregon-redis.render.com:6379'
+  connectionString: 'postgres://original:fSuZdEE7T6fTqVCOlEobSioKlfwR4Rrb@dpg-ckdeitsgonuc73cmsucg-a/original_db'
 })
+fastify.register(require('@fastify/redis'), {
+  url: 'redis://red-cksjdg6nfb1c73c8tgpg:6379'
+})
+//external render acess
+// fastify.register(require('@fastify/postgres'), {
+//     connectionString: 'postgres://original:fSuZdEE7T6fTqVCOlEobSioKlfwR4Rrb@dpg-ckdeitsgonuc73cmsucg-a.oregon-postgres.render.com/original_db?ssl=true'
+//   })
+// fastify.register(require('@fastify/redis'), {
+//   url: 'rediss://red-cksjdg6nfb1c73c8tgpg:eEjoQXin0xOlVfhsOu26xy3BpIjjdgul@oregon-redis.render.com:6379'
+// })
 const PlayerPoolManager = require('./playerPoolManager');
 // const TableManager = require('./tableManager');
 const User = require('./user');
