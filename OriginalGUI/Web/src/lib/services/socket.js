719894851b850a45ec7dd76d85797daf5873fa11
danielgaban <91639510+danielgaban@users.noreply.github.com>
2023-11-17 10:23:17 -0400

    fix(production): prepare to production

--- OriginalGUI/Web/src/lib/services/socket.js
+++ OriginalGUI/Web/src/lib/services/socket.js
@@ -2,7 +2,13 @@ import userStore from '$lib/stores/userStore';
 import io from 'socket.io-client';
 
 // const socket = io('http://192.168.237.73:3000');
-const socket = io('http://192.168.1.110:3000');
+// const socket = io('http://192.168.1.110:3000');
+const socket = io('https://originaltrial.onrender.com', {
+  reconnection: true,
+  reconnectionAttempts: 30,
+  reconnectionDelay: 1000,  // 1 segundo
+  timeout: 20000,
+}); // Replace with your server's address
 
 socket.on('connect', () => {
 	console.log('User connected on socketID:', socket.id);
