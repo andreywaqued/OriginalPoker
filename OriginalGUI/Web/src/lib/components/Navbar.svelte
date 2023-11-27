704d713f426a480f1edc93d753aeae1fa725fffb
danielgaban <91639510+danielgaban@users.noreply.github.com>
2023-11-27 17:41:20 -0400

    feat(mobile-playerwarning): added exclamation warning when user have an action on that table

--- OriginalGUI/Web/src/lib/components/Navbar.svelte
+++ OriginalGUI/Web/src/lib/components/Navbar.svelte
@@ -13,29 +13,32 @@
 </script>
 
 <div class="flex h-14 w-full items-center gap-x-2 overflow-x-auto bg-gray-darkest px-2">
-	{#each Object.entries({ lobby: {}, ...$userStore.players }) as [id, player] (id)}
+	{#each Object.entries({ lobby: {}, ...$userStore?.players }) as [id, player] (id)}
 		<button
 			on:click={() => handleSelected(id)}
 			class:!bg-gray={$navSelectedItemStore === id}
-			class="rounded bg-gray-dark px-2 py-1 text-sm font-bold uppercase text-white"
+			class="relative rounded bg-gray-dark px-2 py-1 text-sm font-bold uppercase text-white"
 		>
-				{#if id === 'lobby'}
-					<p class="whitespace-nowrap">
-						Lobby
-					</p>
-				{:else}
-					{#if !player || player.cards.length === 0 || $navSelectedItemStore === id}
-						<p class="whitespace-nowrap">
-							{$gamesAvailable[player.poolID].gameTitle}
-						</p>
-					{:else}
-						<div class="grid grid-cols-2 aspect-video h-5">
-							{#each player.cards as card}
-								<Card cardString={card} deck={'boardDeck'} />
-							{/each}
-						</div>
-					{/if}
+			{#if id === 'lobby'}
+				<p class="whitespace-nowrap">Lobby</p>
+			{:else if !player || player.cards.length === 0 || $navSelectedItemStore === id}
+				<p class="whitespace-nowrap">
+					{$gamesAvailable[player.poolID].gameTitle}
+				</p>
+			{:else}
+				<div class="grid aspect-video h-5 grid-cols-2">
+					{#each player.cards as card}
+						<Card cardString={card} deck={'boardDeck'} />
+					{/each}
+				</div>
+				{#if player.possibleActions.length > 0}
+					<div
+						class="absolute z-[1000] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-2 border-amber-700 bg-amber-600 px-1.5 text-xs"
+					>
+						!
+					</div>
 				{/if}
+			{/if}
 		</button>
 	{/each}
 </div>
