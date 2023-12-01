03251c3ac0b697b63d65cc54da732e5df46b28af
danielgaban <91639510+danielgaban@users.noreply.github.com>
2023-12-01 13:50:09 -0400

    feat(mobile-banner): chip have been added and some refactor

--- OriginalGUI/Web/tailwind.config.js
+++ OriginalGUI/Web/tailwind.config.js
@@ -6,7 +6,8 @@ export default {
 			colors: {
 				'gray-darkest': 'rgb(25,25,25)',
 				'gray-dark': 'rgb(49,49,49)',
-				'gray': 'rgb(69,69,69)'
+				'gray': 'rgb(69,69,69)',
+				'light-gray': 'rgb(100,100,100)'
 			},
 			fontSize: {
 				'2xs': "0.5rem"
